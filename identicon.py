945d5dddd1d5521f055763e8914704390fcc4c9e
Adithyan-KV <adithyankv007@gmail.com>
2020-08-15 03:13:43 +0530

    Implemented generation of random image matrix
     -A random 5x5 image matrix can now be generated with individual pixels
      turned on or off based on the hash of the username.

--- identicon.py
+++ identicon.py
@@ -1,13 +1,16 @@
+import hashlib
+
+
 def main():
-    get_color_from_hash(abs(hash("random_strin1")))
+    image_data = ImageDataGenerator('random dude')
 
 
-def get_color_from_hash(hash, bitdepth=8):
+def get_color_from_hash(hash_value, bitdepth=8):
     bits_per_channel = 2**bitdepth
     # generate deterministic values for each channel from the random hash
-    red_value = hash % bits_per_channel
-    blue_value = (hash % 1000) % bits_per_channel
-    green_value = ((hash % 1000000)//1000) % bits_per_channel
+    red_value = hash_value % bits_per_channel
+    blue_value = (hash_value % 1000) % bits_per_channel
+    green_value = ((hash_value % 1000000)//1000) % bits_per_channel
     hex_color_code = rgb_to_hex(red_value, green_value, blue_value)
 
     print(hex_color_code)
@@ -21,6 +24,36 @@ def rgb_to_hex(r, g, b):
     hex_code = f'#{hex_r}{hex_g}{hex_b}'
 
     return hex_code
+
+
+class ImageDataGenerator():
+    def __init__(self, username):
+        # sha1 hash used over default hash in python as default hash is salted
+        hash_hex = hashlib.sha1(username.encode('utf8')).hexdigest()
+        self.hash_value = int(hash_hex, 16)
+        self.pixel_matrix = self.pixel_matrix_from_hash(self.hash_value)
+        print(self.pixel_matrix)
+
+    def pixel_matrix_from_hash(self, hash_value):
+        # iterate through the hash and set individual pixels to on or off
+        # depending on whether has value at index is odd or even
+        pixel_matrix = [[0 for i in range(5)] for j in range(5)]
+        # first 25 characters of the hash to enumerate over
+        hash_string = str(hash_value).strip('-')[:24]
+        for index, digit in enumerate(hash_string):
+            x_index = index % 5
+            y_index = index//5
+            if y_index % 5 < 3:
+                y_mirror_index = 4-y_index
+                if int(digit) % 2 == 0:
+                    # mirror values about y axis at center
+                    pixel_matrix[x_index][y_index] = 0
+                    pixel_matrix[x_index][y_mirror_index] = 0
+                else:
+                    pixel_matrix[x_index][y_index] = 1
+                    pixel_matrix[x_index][y_mirror_index] = 1
+
+        return pixel_matrix
 
 
 if __name__ == "__main__":
