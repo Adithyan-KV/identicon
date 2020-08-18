a8f284b12719b669416d0261ea7adb90a005ea82
Adithyan-KV <adithyankv007@gmail.com>
2020-08-19 02:16:15 +0530

    Added a script to generate identicons from a namelist for testing
     -A random name list was used to generate identicons so as to visually
      inspect a large sample

---
+++ random_test_identicons.py
@@ -0,0 +1,11 @@
+import identicon
+
+
+def main():
+    with open('name_list.txt') as name_list:
+        for name in name_list:
+            identicon.generate_identicon(name)
+
+
+if __name__ == "__main__":
+    main()
