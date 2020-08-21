bc46f9b06c506e16effb6cf81bdcfbd0e313824f
Adithyan-KV <adithyankv007@gmail.com>
2020-08-22 00:21:30 +0530

    Changes to test script
     -The test script was changed to account for custom sizes in identicon
      script.

--- random_test_identicons.py
+++ random_test_identicons.py
@@ -4,8 +4,7 @@ import identicon
 def main():
     with open('name_list.txt') as name_list:
         for name in name_list:
-            print(name.strip())
-            # identicon.generate_identicon(name.strip())
+            identicon.generate_identicon(name.strip(), 256)
 
 
 if __name__ == "__main__":
