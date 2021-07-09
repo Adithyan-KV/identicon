88404e4a9f22a75e951c390562563225991c521a
Adithyan-KV <adithyankv007@gmail.com>
2021-07-09 14:44:52 +0530

    Made more efficient by using numpy methods

--- identicon.py
+++ identicon.py
@@ -3,9 +3,10 @@ import hashlib
 import numpy as np
 import os
 from PIL import Image, ImageOps
+import matplotlib.pyplot as plt
 
 
-def generate_identicon(username, size=256, path=os.getcwd(), file_format='jpeg'):
+def generate_identicon(username, size=256, path=os.getcwd(), file_format='png'):
     """Generate an identicon and save as image
 
     Args:
@@ -27,6 +28,8 @@ def generate_identicon(username, size=256, path=os.getcwd(), file_format='jpeg')
     content = size-padding
     resized_image = image.resize((content, content), resample=Image.NEAREST)
     padded_image = ImageOps.expand(resized_image, padding, (220, 220, 220))
+    plt.imshow(padded_image)
+    plt.show()
     save_image(padded_image, path, username, file_format)
 
 
@@ -64,53 +67,13 @@ def generate_image_data(hash_value):
         of fully colored image
     """
     pixel_matrix = pixel_matrix_from_hash(hash_value)
-    image_matrix = np.array(pixel_matrix)
     color = random_color_from_hash(hash_value)
-    bgcolor = [220, 220, 220]  # RGB values
-    img_bw = np.array([image_matrix for i in range(3)])
-    img_fg_colored = add_foreground_color(img_bw, color)
-    img_colored = add_background_color(img_fg_colored, bgcolor)
-
-    return img_colored
-
-
-def add_background_color(image_matrix, color):
-    """Sets the background pixels of the image to specified bg color
-
-    Args:
-        matrix (np.array): The image data with background pixels as 0
-        color (array): The background color array in RGB format
-
-    Returns:
-        image_matrix (np.array): fully colorized image data with background 
-        colors applied
-    """
-    for i in range(5):
-        for j in range(5):
-            if image_matrix[i, j, 0] == 0:
-                image_matrix[i, j] = color
-
-    return image_matrix
-
-
-def add_foreground_color(matrix, color):
-    """Takes in a 3d matrix with on or off pixel values and applies the color
-
-    Args:
-        matrix(nparray): 3D np array showing on and off pixel values
-        color(tuple): the(R, G, B) tuple
-
-    Returns:
-        nparray: returns a 3D nparray that represents an image file, with the
-        3 dimensions representing the RGB channels
-    """
-    for i in range(3):
-        matrix[i, :, :] = np.round(matrix[i, :, :]*color[i]*255)
-
-    reshaped_matrix = np.transpose(matrix, (1, 2, 0))
-
-    return reshaped_matrix
-
+    bg_color = np.array([220, 220, 220])  # RGB values
+    bw_img = np.repeat(pixel_matrix[:,:,np.newaxis],3,axis=2)
+    fg_img = bw_img*color*255
+    bg_img = (bw_img==0)*bg_color
+    final_img = fg_img+bg_img
+    return final_img
 
 def pixel_matrix_from_hash(hash_value):
     """Sets pixels of the image to on or off based on hash value.
@@ -121,7 +84,7 @@ def pixel_matrix_from_hash(hash_value):
     Returns:
         np.array: A symmetric black and white pixel map matrix
     """
-    pixel_matrix = [[0 for i in range(5)] for j in range(5)]
+    pixel_matrix = np.zeros((5,5))
     # first 25 characters of the hash to enumerate over
     hash_string = str(hash_value).strip('-')[:14]
     for index, digit in enumerate(hash_string):
@@ -130,11 +93,11 @@ def pixel_matrix_from_hash(hash_value):
         mirror_column = 4-column
         if int(digit) % 2 == 0:
             # mirror values about y axis at center
-            pixel_matrix[row][column] = 1
-            pixel_matrix[row][mirror_column] = 1
+            pixel_matrix[row,column] = 1
+            pixel_matrix[row,mirror_column] = 1
         else:
-            pixel_matrix[row][column] = 0
-            pixel_matrix[row][mirror_column] = 0
+            pixel_matrix[row,column] = 0
+            pixel_matrix[row,mirror_column] = 0
 
     return pixel_matrix
 
@@ -153,9 +116,9 @@ def random_color_from_hash(hash_value):
     # saturation, value hardcoded for consistency in palette
     saturation = 0.78
     value = 0.92
-    color_rgb = colorsys.hsv_to_rgb(hue, saturation, value)
+    color_rgb = np.array(colorsys.hsv_to_rgb(hue, saturation, value))
     return color_rgb
 
 
 if __name__ == "__main__":
-    generate_identicon('testname', file_format='bleh')
+    generate_identicon('phoney badger')
